783527d5aa283e72b6a68ee5a057cc427f1d8b3b
Thanh-Vy Duong <dng.thanhvy@gmail.com>
2021-02-23 10:23:05 +0100

    added Demographic Data Analyzer

--- Demographic Data Analyzer/demographic_data_analyzer.py
+++ Demographic Data Analyzer/demographic_data_analyzer.py
@@ -3,47 +3,51 @@ import pandas as pd
 
 def calculate_demographic_data(print_data=True):
     # Read data from file
-    df = None
+    df = pd.read_csv('adult.data.csv')
 
     # How many of each race are represented in this dataset? This should be a Pandas series with race names as the index labels.
-    race_count = None
+    race_count = df['race'].value_counts()
 
     # What is the average age of men?
-    average_age_men = None
+    age_series = df['age']
+    men_series = df['sex'] == 'Male'
+    average_age_men = round(age_series[men_series].mean(), 1)
 
     # What is the percentage of people who have a Bachelor's degree?
-    percentage_bachelors = None
+    education_bachelors = df[df['education'] == 'Bachelors']
+    percentage_bachelors = round(100*len(education_bachelors)/len(df), 1)
 
     # What percentage of people with advanced education (`Bachelors`, `Masters`, or `Doctorate`) make more than 50K?
     # What percentage of people without advanced education make more than 50K?
 
     # with and without `Bachelors`, `Masters`, or `Doctorate`
-    higher_education = None
-    lower_education = None
+    higher_education = df[df['education'].isin(['Bachelors', 'Masters', 'Doctorate'])]
+    lower_education = df[~df['education'].isin(['Bachelors', 'Masters', 'Doctorate'])]
 
     # percentage with salary >50K
-    higher_education_rich = None
-    lower_education_rich = None
+    higher_education_rich = round(100*len(higher_education[higher_education['salary'] == ">50K"])/len(higher_education), 1)
+    lower_education_rich = round(100*len(lower_education[lower_education['salary'] == ">50K"])/len(lower_education), 1)
 
     # What is the minimum number of hours a person works per week (hours-per-week feature)?
-    min_work_hours = None
+    min_work_hours = df['hours-per-week'].min()
 
     # What percentage of the people who work the minimum number of hours per week have a salary of >50K?
-    num_min_workers = None
+    min_work_hours_mask = df[df['hours-per-week'] == min_work_hours]
+    num_min_workers = len(min_work_hours_mask)
 
-    rich_percentage = None
+    rich_percentage = round(100*len(df[(df['hours-per-week'] == min_work_hours) & (df['salary'] == '>50K')])/num_min_workers, 1)
 
     # What country has the highest percentage of people that earn >50K?
-    highest_earning_country = None
-    highest_earning_country_percentage = None
+    highest_earning_country = (df.loc[df['salary'] == ">50K", 'native-country'].value_counts() / df['native-country'].value_counts()).fillna(0).sort_values(ascending=False).index[0]
+    highest_earning_country_percentage = round(100*len(df[(df['native-country'] == highest_earning_country) & (df['salary'] == '>50K')]) / len(df[df['native-country'] == highest_earning_country]), 1)
 
     # Identify the most popular occupation for those who earn >50K in India.
-    top_IN_occupation = None
+    top_IN_occupation = df[(df['salary'] == ">50K") & (df['native-country'] == "India")]["occupation"].value_counts().idxmax()
 
     # DO NOT MODIFY BELOW THIS LINE
 
     if print_data:
-        print("Number of each race:\n", race_count) 
+        print("Number of each race:\n", race_count)
         print("Average age of men:", average_age_men)
         print(f"Percentage with Bachelors degrees: {percentage_bachelors}%")
         print(f"Percentage with higher education that earn >50K: {higher_education_rich}%")
@@ -64,6 +68,6 @@ def calculate_demographic_data(print_data=True):
         'rich_percentage': rich_percentage,
         'highest_earning_country': highest_earning_country,
         'highest_earning_country_percentage':
-        highest_earning_country_percentage,
+            highest_earning_country_percentage,
         'top_IN_occupation': top_IN_occupation
     }
--- Demographic Data Analyzer/main.py
+++ Demographic Data Analyzer/main.py
@@ -5,5 +5,5 @@ from unittest import main
 # Test your function by calling it here
 demographic_data_analyzer.calculate_demographic_data()
 
-# Run unit tests automatically
-main(module='test_module', exit=False)
+"""# Run unit tests automatically
+main(module='test_module', exit=False)"""
